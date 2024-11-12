6226b24d99a389efba298bb6242bdc6dfa4f06ce
misunderstood <2djohar@gmail.com>
2024-11-12 20:28:35 +0200

    updated pdf with qty printing

--- pages/07_Inventory.py
+++ pages/07_Inventory.py
@@ -15,14 +15,11 @@ import os
 from barcode import Code128, Code39
 from barcode.writer import ImageWriter
 
-# import login
-# st.session_state['login'], st.session_state['username']= login.login()
+# import login_google
+# st.session_state['login'] = login_google.login()
 
-import login_google
-st.session_state['login'] = login_google.login()
-
-if st.session_state['login'][0]:
-# if True:
+# if st.session_state['login'][0]:
+if True:
     width = 2.2
     height = 0.4
     options_fnsku = {'module_width':height, 'module_height':width+7, 'font_size':10, 'text_distance':4}
@@ -39,19 +36,16 @@ if st.session_state['login'][0]:
         os.makedirs('barcodes')
 
     def check_skus(sku_list,dictionary):
-        wrong_skus = []
-        for i, sku in enumerate(sku_list):
-            file_sku = dictionary[dictionary['sku'] == sku]
-            if len(file_sku) == 0:
-                wrong_skus.append(sku)
+        test_skus = pd.DataFrame(sku_list, columns=['sku'])
+        dict_skus = dictionary['sku'].unique().tolist()
+        wrong_skus = test_skus[~test_skus['sku'].isin(dict_skus)]
         if len(wrong_skus) > 0:
-            skus_out = '\n'.join(wrong_skus)
+            skus_out = '\n'.join(wrong_skus['sku'].unique())
             st.text(f'The following SKUs were not found:\n{skus_out}')
             return False
-        else:
-            return True
+        return True
         
-    def generate_pdf(fnskus, titles, qty):
+    def generate_pdf(fnsku_dict, mode='combined'):#fnskus, titles, qty):
         #PDF
         pdf_w = 8.5
         pdf_h = 11
@@ -60,34 +54,46 @@ if st.session_state['login'][0]:
         x_coords = [round(x,3) for x in np.arange(xmargin,pdf_w-xmargin,((pdf_w-xmargin)-xmargin)/3)+0.15]
         y_coords = [round(y,3) for y in np.arange(ymargin,pdf_h-ymargin,((pdf_h-ymargin)-ymargin)/10)+0.2]
         pdf = FPDF('P', 'in', 'Letter')
-        pdf.add_page()
-        # pdf.set_stretching(70.0)
-        pdf.set_font('Arial', style ='', size = 8)
-        
-        #generate fnsku barcodes from the list
-        ix = 0
-        iy = 0
-        for i, fnsku in enumerate(fnskus):
-            with open(rf'barcodes\{fnsku}.png', 'wb') as f:
-                Code128(str(fnsku), writer = ImageWriter()).write(f, options = options_fnsku)
-            title_str = textwrap.wrap(titles[i], width = 40)
-            number_barcodes = qty[i]
-            for n in range(number_barcodes):
+        if mode == 'combined':
+            pdf.add_page()
+            ix = 0
+            iy = 0
+        for record in fnsku_dict:
+            if mode == 'separate':
+                ix = 0
+                iy = 0
+                pdf.add_page()
+                pdf.set_font('Arial', style ='', size = 24)
+                pdf.text(1,pdf_h/2-0.8,record['sku'])
+                pdf.text(1,pdf_h/2-0.4,record['upc'])
+                pdf.text(1,pdf_h/2+0.0,record['fnsku'])
+                pdf.text(1,pdf_h/2+0.4,record['short_title'])
+                pdf.text(1,pdf_h/2+0.8,f"# of labels: {record['qty']}")
+
+            with open(f'barcodes/{record['fnsku']}.png', 'wb') as f:
+                Code128(str(record['fnsku']), writer = ImageWriter()).write(f, options = options_fnsku)
+            title_str = textwrap.wrap(record['short_title'], width = 40)
+            number_barcodes = record['qty']
+            
+            if mode == 'separate':
+                pdf.add_page()
+            pdf.set_font('Arial', style ='', size = 8)
+
+            for _ in range(number_barcodes):
                 for p,s in enumerate(title_str):
                     pdf.text(x_coords[ix]+.1+(p/3), y_coords[iy]+0.5+(p/10), s)
                 pdf.text(x_coords[ix]+.1, y_coords[iy]+0.6, 'New')
-                pdf.image(rf'barcodes\{fnsku}.png', x = x_coords[ix], y = y_coords[iy], w = width, h = height, type = '', link = '')
+                pdf.image(f'barcodes/{record['fnsku']}.png', x = x_coords[ix], y = y_coords[iy], w = width, h = height, type = '', link = '')
+                
                 if ix <2:
                     ix += 1
                 else:
                     ix = 0
                     iy +=1
-                if iy <10:
-                    pass
-                else:
+                if iy == 10:
                     iy = 0
-                    pdf.add_page()
-        
+                    if number_barcodes % 30 != 0 or mode == 'combined':
+                        pdf.add_page()
         # pdf.output('barcodes.pdf', 'F')
         return pdf
 
@@ -114,37 +120,24 @@ if st.session_state['login'][0]:
             pdf_h = 1.25
             xmargin = 0.05
             ymargin = 0.2
-            # x_coords = [round(x,3) for x in np.arange(xmargin,pdf_w-xmargin,((pdf_w-xmargin)-xmargin)/3)+0.15]
-            # y_coords = [round(y,3) for y in np.arange(ymargin,pdf_h-ymargin,((pdf_h-ymargin)-ymargin)/10)+0.2]
             pdf = FPDF('L', 'in', (1.25,4))
         
         #start barcode pages
         pdf.add_page()
         pdf.set_font('Arial', style ='', size = 11)
-        # skus = dictionary['sku']#.unique()
-        # upcs = dictionary['upc']#.dropna().unique()
-        # if len(upcs) == 0:
-        #     del file['UPC']
-        #     d = pd.read_excel('Dictionary.xlsx', usecols = ['FNSKU','UPC'])
-        #     d = d.drop_duplicates('FNSKU', keep = 'last')
-        #     file = pd.merge(file, d, how = 'left', on = 'FNSKU')
-        #generate itf barcodes from the list
+
         ix = 0
         iy = 0
         for i, sku in enumerate(sku_list):
-            # print(f'Printing {sku}')
             file_sku = dictionary[dictionary['sku'] == sku]
             sku_str = textwrap.wrap(sku, width = 25)
             upc = f"{leading}{file_sku['upc'].values[0]}"
-            # quantity = file_sku['Quantity'].values[0]
-            with open(rf'barcodes\{sku.replace("-","_")}.png', 'wb') as f:
+            with open(f'barcodes/{sku.replace("-","_")}.png', 'wb') as f:
                 Code39(str(upc), writer = ImageWriter(),add_checksum = False).write(f, options = options_itf)
-            # time.sleep(0.5)
-            # for q in qty:
             if layout == 'Letter':
                 for p,s in enumerate(sku_str):
                     pdf.text(x_coords[ix]+.1, y_coords[iy]+(p/7), s)
-                pdf.image(rf'barcodes\{sku.replace("-","_")}.png', x = x_coords[ix], y = y_coords[iy]+0.2, w = width, h = height, type = '', link = '')
+                pdf.image(f'barcodes/{sku.replace("-","_")}.png', x = x_coords[ix], y = y_coords[iy]+0.2, w = width, h = height, type = '', link = '')
                 if ix <2:
                     ix += 1
                 else:
@@ -161,7 +154,7 @@ if st.session_state['login'][0]:
             elif layout == 'Zebra':
                 for p,s in enumerate(sku_str):
                     pdf.text(xmargin+0.5, ymargin+(p/7), s)
-                pdf.image(rf'barcodes\{sku.replace("-","_")}.png', x = xmargin+0.3, y = ymargin+0.2, w = width, h = height, type = '', link = '')
+                pdf.image(f'barcodes/{sku.replace("-","_")}.png', x = xmargin+0.3, y = ymargin+0.2, w = width, h = height, type = '', link = '')
                 # ymargin += 0.2
                 pdf.add_page()
         return pdf
@@ -175,19 +168,10 @@ if st.session_state['login'][0]:
             os.removedirs('barcodes')
         return None
 
-
-
-    # col1, col2, = st.columns([10,3])
-
     with col3:
         # @st.cache_data(show_spinner=False)
         def download_dictionary(full = False):
             st.session_state.market = st.radio('Select marketplace',['US','CA','EU','UK'], horizontal=True)
-            # client = gc.gcloud_connect()
-            # sql = '''SELECT * FROM `auxillary_development.dictionary`'''
-            # query_job = client.query(sql)  # Make an API request.
-            # dictionary = query_job.result().to_dataframe()
-            # client.close()
             dictionary = gc.pull_dictionary(combine = False, market = st.session_state.market, full = full)
             output = BytesIO()
             with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
@@ -205,7 +189,7 @@ if st.session_state['login'][0]:
         if col3.button('Clear'):
             remove_images()
     with col2:
-        barcode_type = col2.radio('Select barcode type:',['FNSKU','ITF14'])
+        barcode_type = col2.radio('Select barcode type:',['FNSKU','ITF14','UPC upload with qty'])
         
         if barcode_type == 'ITF14':
             layout = col2.radio('Layout type',['Letter','Zebra'])
@@ -214,27 +198,46 @@ if st.session_state['login'][0]:
         elif barcode_type == 'FNSKU':
             layout = 'Letter'
             qty_type = col2.radio('Labels per sheet',[1,30])
+        elif barcode_type == 'UPC upload with qty':
+            layout = 'Letter'
 
     with col1:
-        skus = st.text_area('Input SKUs', height = 300, help = 'Input a list of SKUs you want to generate barcodes for').split('\n')
-        sku_list = [x for x in skus if x != '']
+        if barcode_type in ('ITF14','FNSKU'):
+            skus = st.text_area('Input SKUs', height = 300, help = 'Input a list of SKUs you want to generate barcodes for').split('\n')
+            sku_list = [x for x in skus if x != '']
+        elif barcode_type == 'UPC upload with qty':
+            qty_file_obj = st.file_uploader('Upload file with SKUs and quantities (make sure it has columns "sku" and "qty")')
+            if qty_file_obj:
+                qty_file = pd.read_excel(qty_file_obj)
+                sku_list = qty_file['sku'].unique().tolist()
         if col1.button('Create barcodes') and len(sku_list) > 0:
             with st.spinner('Please wait'):
                 dictionary = gc.pull_dictionary(combine = True)
                 check_skus(sku_list,dictionary)
                 st.session_state.file = dictionary[dictionary['sku'].isin(sku_list)].reset_index()
                 del st.session_state.file['index']
-                fnskus = st.session_state.file['fnsku']
-                titles = st.session_state.file['short_title']
-                upcs = st.session_state.file['upc']
-                skus = st.session_state.file['sku']
-                qty = [qty_type]*len(sku_list)
-                if barcode_type == 'FNSKU':
-                    st.session_state.pdf = generate_pdf(fnskus, titles, qty)
+
+                if barcode_type != 'UPC upload with qty':
+                    st.session_state.file['qty'] = qty_type
+                else:
+                    st.session_state.file = pd.merge(st.session_state.file, qty_file, how = 'outer', on = 'sku')
+                
+                fnsku_dict = st.session_state.file.to_dict(orient='records')
+                
+                
+                # fnskus = st.session_state.file['fnsku']
+                # titles = st.session_state.file['short_title']
+                # upcs = st.session_state.file['upc']
+                # skus = st.session_state.file['sku']
+                # qty = [qty_type]*len(sku_list)
+                if barcode_type in ('FNSKU','UPC upload with qty'):
+                    st.session_state.pdf = generate_pdf(fnsku_dict, mode='separate' if barcode_type == 'UPC upload with qty' else 'combined')
                     st.session_state.file_name = 'Individual barcodes.pdf'
                 elif barcode_type == 'ITF14':
+                    qty = qty_type
                     st.session_state.pdf = generate_itf(sku_list,dictionary, layout, qty, leading = leading)
                     st.session_state.file_name = 'ITF14 barcodes.pdf'
+                    
         if 'pdf' in st.session_state and st.session_state['pdf'] is not None:
             st.session_state.pdf.output('barcodes/barcodes.pdf', 'F')
             with open('barcodes/barcodes.pdf', "rb") as pdf_file:
