735d1c4d8050062e92be97052f974d7d16fcdd74
misunderstood <2djohar@gmail.com>
2024-12-25 16:21:06 +0200

    adding new sales estimator

--- pages/02_Sales_Estimator.py
+++ pages/02_Sales_Estimator.py
@@ -13,23 +13,23 @@ st.session_state['login'] = login_google.login()
 if st.session_state['login'][0]:
     st.subheader('_Get ASIN sales_')
 
-input_area = st.container()
-product_area = st.container()
-product_title_area, product_image_area = product_area.columns([3,1])
-df_area = st.container()
-plot_area = st.container()
+    input_area = st.container()
+    product_area = st.container()
+    product_title_area, product_image_area = product_area.columns([3,1])
+    df_area = st.container()
+    plot_area = st.container()
 
-asin = input_area.text_input('ASIN', max_chars=10, key = 'ASIN', help='Enter ASIN or Amazon link to check latest stats')
-submit_button = input_area.button('Submit')
-if submit_button and asin:
-    asin_clean = re.search('[A-Z0-9]{10}', asin).group()
-    product = KeepaProduct(asin_clean)
-    try:
-        product.generate_daily_sales()
-        product_title_area.write(product)
-        product_image_area.image(product.image)
-        product.get_last_days(days=360)
-        df_area.write('Latest price history and average sales per day:')
-        df_area.dataframe(product.last_days)
-    except Exception as e:
-        st.write(e)
+    asin = input_area.text_input('ASIN', max_chars=10, key = 'ASIN', help='Enter ASIN or Amazon link to check latest stats')
+    submit_button = input_area.button('Submit')
+    if submit_button and asin:
+        asin_clean = re.search('[A-Z0-9]{10}', asin).group()
+        product = KeepaProduct(asin_clean)
+        try:
+            product.generate_daily_sales()
+            product_title_area.write(product)
+            product_image_area.image(product.image)
+            product.get_last_days(days=360)
+            df_area.write('Latest price history and average sales per day:')
+            df_area.dataframe(product.last_days)
+        except Exception as e:
+            st.write(e)
