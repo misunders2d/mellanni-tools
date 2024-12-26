5de8e3d603f711922dcbeddd1a8ae4706ae65c91
misunderstood <2djohar@gmail.com>
2024-12-26 19:35:16 +0200

    change in google login confirmation

--- login_google.py
+++ login_google.py
@@ -53,8 +53,7 @@ def login():
             time.sleep(0.5)
             st.rerun()
     else:
-        st.write("You are logged in!")
-        st.write('Your email:',st.session_state["auth"])
+        st.write(f"You are logged in as {st.session_state["auth"]}")
         if st.session_state['auth'] in PREAUTHORIZED_EMAILS:
             return True, st.session_state["auth"]
         # st.write(st.session_state["token"])
