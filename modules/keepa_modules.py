7d403d2727a0c881312da683e65f1f9f827a4bc6
misunderstood <2djohar@gmail.com>
2025-02-13 19:08:17 +0200

    coupons separated explicitly into % off and $ off

--- modules/keepa_modules.py
+++ modules/keepa_modules.py
@@ -145,16 +145,19 @@ class KeepaProduct:
         if coupons:
             times = [self.convert_time(x) for x in coupons[::3]]
             discounts = coupons[1::3]
-            perc_off = [x if x < 0 else 0 for x in discounts]
-            money_off = [-x/100 if x > 0 else 0 for x in discounts]
+            perc_off = [x if x < 0 else 0 for x in discounts] # separate % off discounts
+            money_off = [-x/100 if x > 0 else 0 for x in discounts] # separate $ off discounts
             sns_coupons = coupons[2::3]
+            sns_perc_off = [x if x < 0 else 0 for x in sns_coupons] # separate % off sns
+            sns_money_off = [-x/100 if x > 0 else 0 for x in sns_coupons] # separate $ off sns
+            
             coupon_history = pd.DataFrame(
-                data=list(zip(perc_off, money_off, sns_coupons)),
+                data=list(zip(perc_off, money_off, sns_perc_off, sns_money_off)),
                 index=times,
-                columns=['% off','$ off','SNS']
+                columns=['% off','$ off','SNS %', 'SNS $']
                 )
         else:
-            coupon_history = pd.DataFrame([[0,0,0]], index =[self.last_sales_date], columns=['% off','$ off','SNS'])
+            coupon_history = pd.DataFrame([[0,0,0]], index =[self.last_sales_date], columns=['% off','$ off','SNS %', 'SNS $'])
         
         sales_history = pd.merge(sales, coupon_history, how='outer', left_index=True, right_index=True).ffill()
         return sales_history
@@ -177,10 +180,8 @@ class KeepaProduct:
         bsr = self.data[0].get('data',{}).get('df_SALES', pd.DataFrame([nan], index=[self.last_sales_date], columns=['BSR'])).replace(-1,nan)
         bsr = bsr.rename(columns = {'value':'BSR'})
         sales_history = pd.merge(sales_history, bsr, how='outer', left_index=True, right_index=True).ffill()
-        sales_history['final price'] = sales_history['full price'] * (1+ sales_history['% off']/100) * (1+ sales_history['SNS']/100)- sales_history['$ off']
-        sales_history.loc[
-            sales_history['SNS']>0, 'final price'
-            ] = sales_history['full price'] * (1+ sales_history['% off']/100) - (sales_history['SNS']/100)- sales_history['$ off']
+        sales_history['final price'] = (sales_history['full price'] - sales_history['$ off'] - sales_history['SNS $']) * (1+ sales_history['% off']/100) * (1+ sales_history['SNS %']/100)
+        
         sales_history.loc[sales_history['LD'] != 0, 'final price'] = sales_history['LD']
         return sales_history
        
@@ -243,7 +244,7 @@ class KeepaProduct:
         minutely_history['date'] = minutely_history.index.date
         self.pivot = minutely_history.pivot_table(
             values = [
-                'full price','% off', '$ off', 'SNS', 'LD', 'final price',
+                'full price','% off', '$ off', 'SNS %', 'SNS $', 'LD', 'final price',
                 'sales min','sales max','BSR'
                 ],
             index = 'date',
@@ -251,7 +252,8 @@ class KeepaProduct:
                 'full price':'mean',
                 '% off':'min',
                 '$ off':'min',
-                'SNS':'min',
+                'SNS %':'min',
+                'SNS $':'min',
                 'LD':'max',
                 'final price':'mean',
                 'sales min':'sum',
@@ -261,10 +263,7 @@ class KeepaProduct:
             )
         self.short_history['LD'] = self.short_history['LD'].replace(0,nan)
         self.short_history['full price'] = self.short_history['full price'].replace(-1, nan)
-        self.short_history['coupon'] = (minutely_history['full price'] - minutely_history['$ off']) * (1+minutely_history['% off']/100) * (1+minutely_history['SNS']/100)
-        self.short_history.loc[
-            self.short_history['SNS']>0, 'coupon'
-            ] = (minutely_history['full price'] - minutely_history['$ off'] - (minutely_history['SNS']/100)) * (1+minutely_history['% off']/100)
+        self.short_history['coupon'] = (minutely_history['full price'] - minutely_history['$ off'] - minutely_history['SNS $']) * (1+minutely_history['% off']/100) * (1+minutely_history['SNS %']/100)
         self.short_history.loc[self.short_history['coupon']==self.short_history['full price'], 'coupon'] = nan
         self.pivot = self._format_numbers(self.pivot)
         self.pivot = self.pivot.replace(0,nan)
