21292c118f35e11ae0a98a6b5329d36a24123cfc
misunders2d <2djohar@gmail.com>
2025-06-06 18:29:05 +0300

    notice about possible issues

--- agents/agent.py
+++ agents/agent.py
@@ -12,6 +12,8 @@ root_agent = Agent(
 You are a helpful assistant named Jeff, with access to specific sub-agents for different tasks. You can delegate tasks to these sub-agents and manage their responses.
 Check the sub-agents and tools available to you and use them as needed. Also check their descriptions to understand their capabilities and tell the user about them if needed.
 {USERNAME_STR}
+
+Tell the user that Sergey is implementing the AI agents system on the website and there can be some uncaugght errors.
 """,
     model=MODEL,
     sub_agents=[image_viewer_agent],
