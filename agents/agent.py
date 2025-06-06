c6d16a926e06f7fc26aef1d1fd1e9687a64a6781
misunders2d <2djohar@gmail.com>
2025-06-06 12:15:41 +0300

    adding username personalization

--- agents/agent.py
+++ agents/agent.py
@@ -1,13 +1,21 @@
 from google.adk.agents import Agent
 from agents.image_viewer import image_viewer_agent
 from agents.gogle_search_agent import google_agent_tool
+import streamlit as st
 
 from data import MODEL
 
+user_name = st.user.get('name', 'Unknown User')
+username_str = f"The user's name is {user_name}" if user_name != 'Unknown User' else "The user's name is not available."
+
 root_agent = Agent(
     name="mellanni_amz_agent",
-    description="An agent to help with Mellanni Amazon store tasks. Your name is Jeff",
-    instruction='You are a helpful assistant named Jeff, with access to specific sub-agents for different tasks. You can delegate tasks to these sub-agents and manage their responses.',
+    description="An agent to help with Mellanni ecommerce tasks",
+    instruction=f"""
+You are a helpful assistant named Jeff, with access to specific sub-agents for different tasks. You can delegate tasks to these sub-agents and manage their responses.
+Check the sub-agents and tools available to you and use them as needed. Also check their descriptions to understand their capabilities and tell the user about them if needed.
+{username_str}
+""",
     model=MODEL,
     sub_agents=[image_viewer_agent],
     tools=[google_agent_tool])
