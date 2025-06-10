0d0c2f580a25a4cbb795636bf75b92b78ba0c4d2
misunders2d <2djohar@gmail.com>
2025-06-10 14:12:54 +0300

    moving agents to functions, to recreate multiple agents of the same type

--- agents/agent.py
+++ agents/agent.py
@@ -10,13 +10,15 @@ def create_root_agent():
     root_agent = Agent(
         name="mellanni_amz_agent",
         description="An agent to help with Mellanni ecommerce tasks",
-        instruction=f"""
-    You are a helpful assistant named Jeff, with access to specific sub-agents for different tasks. You can delegate tasks to these sub-agents and manage their responses.
-    Check the sub-agents and tools available to you and use them as needed. Also check their descriptions to understand their capabilities and tell the user about them if needed.
-    {get_username_str()}
-
-    Tell the user that Sergey is implementing the AI agents system on the website and there can be some uncaugght errors.
-    """,
+        instruction= (
+            "You are a helpful assistant named Jeff, with access to specific sub-agents for different tasks.\n"
+            "You can delegate tasks to these sub-agents and manage their responses.\n"
+            "Check the sub-agents and tools available to you and use them as needed. "
+            "Generally you have abilites to search the web using google search, create and improve image and video prompts, view images."
+            "Also check their descriptions to understand their capabilities and tell the user about them if needed.\n"
+            f"{get_username_str()}\n"
+            "Tell the user that Sergey is implementing the AI agents system on the website and there can be some uncaught errors."
+        ),
         model=MODEL,
         sub_agents=[create_image_viewer_agent(), create_creatives_agent()],
         tools=[google_agent_tool]
