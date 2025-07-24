6a666413e43c31af286daad7e400a542aa4ee637
misunderstood <2djohar@gmail.com>
2025-07-24 19:07:10 +0300

    removing meeting summarizer, minor warning fixes

--- pages/06_Tools.py
+++ pages/06_Tools.py
@@ -26,7 +26,7 @@ if login_st():
     st.write(user_email)
 
     with col2:
-        # @st.cache_data(show_spinner=False)
+        @st.cache_data(show_spinner=False, ttl=3600)
         def pull_dictionary():
             client = gc.gcloud_connect()
             sql = '''SELECT * FROM `auxillary_development.dictionary`'''
@@ -62,7 +62,7 @@ if login_st():
                 if sorted_keys:
                     otps = {key: otp(item) for key, item in sorted_keys.items()}
                     output = '\n'.join(f"{k}: ".ljust(50 - len(v)) + v for k, v in sorted(otps.items()))
-                    otp_area = st.text_area('OTPs',output, height=200, key=time.time())
+                    st.text_area(label='OTPs',value=output, height=200, key=str(time.time()))
                     if st.button('Refresh', key = 'OTP refresh'):
                         st.rerun()
             except:
@@ -71,26 +71,26 @@ if login_st():
         with st.expander('Link generator for Seller Central', icon=':material/link:'):
             sc_markets = st.radio('Select marketplace',['US','CA'], horizontal=True, key = 'SC_RADIO')
             domain = 'com' if sc_markets == 'US' else 'ca'
-            result = []
+            links_result = []
 
             def review_links():
                 for a in asin_list:
                     link = f'https://www.amazon.{domain}/product-reviews/'+a+'/ref=cm_cr_arp_d_viewopt_fmt?sortBy=recent&pageNumber=1&formatType=current_format'
-                    result.append(link)
-                return result
+                    links_result.append(link)
+                return links_result
                     
             def sc_links():
                 for a in asin_list:
                     link = f'https://sellercentral.amazon.{domain}/myinventory/inventory?fulfilledBy=all&page=1&pageSize=25&searchField=all&searchTerm={a}&sort=available_desc&status=all&ref_=xx_invmgr_favb_xx'
                     # link = f'https://sellercentral.amazon.{domain}/inventory/ref=xx_invmgr_dnav_xx?tbla_myitable=sort:%7B%22sortOrder%22%3A%22ASCENDING%22%2C%22sortedColumnId%22%3A%22skucondition%22%7D;search:'+a+';pagination:1;'
-                    result.append(link)
-                return result
+                    links_result.append(link)
+                return links_result
 
             def pdp_links():
                 for a in asin_list:
                     link = f'https://www.amazon.{domain}/dp/'+a
-                    result.append(link)
-                return result
+                    links_result.append(link)
+                return links_result
 
             def check_prices():
                 client = gc.gcloud_connect()
@@ -114,21 +114,21 @@ if login_st():
                     sku = dict_asin['SKU'].tolist()#[0]
                     for s in sku:
                         link = f'https://sellercentral.amazon.{domain}/abis/listing/edit?marketplaceID=ATVPDKIKX0DER&ref=xx_myiedit_cont_myifba&sku={s}&asin={a}&productType=HOME_BED_AND_BATH#product_details'
-                        result.append(link)
-                return result
+                        links_result.append(link)
+                return links_result
                 
             def fix_stranded_inventory():
                 for a in asin_list:
                     # dict_asin = dictionary[dictionary['ASIN'] == a]
                     link = f'https://sellercentral.amazon.{domain}/inventory?viewId=STRANDED&ref_=myi_ol_vl_fba&tbla_myitable=sort:%7B%22sortOrder%22%3A%22DESCENDING%22%2C%22sortedColumnId%22%3A%22date%22%7D;search:'+a+';pagination:1;'
-                    result.append(link)
-                return result
+                    links_result.append(link)
+                return links_result
                     
             def order_links():
                 for a in asin_list:
                     link = f'https://sellercentral.amazon.{domain}/orders-v3/search?page=1&q='+a+'&qt=asin'
-                    result.append(link)
-                return result
+                    links_result.append(link)
+                return links_result
             
             functions = {
                 '1 - review links':review_links,
@@ -147,113 +147,12 @@ if login_st():
                 asin_list = [x for x in asins if x != ""]
                 func = functions[option]
                 result = func()
-                if isinstance(result,pd.core.frame.DataFrame):
+                if isinstance(result,pd.DataFrame):
                     result = result.reset_index().drop('index', axis = 1)
                 st.data_editor(result)
 
         with st.expander('Process LD results', icon=':material/electric_bolt:'):
             st.markdown('**Note:** This tool has been replaced with a [Chrome Extension](https://chromewebstore.google.com/detail/amazon-ld-manager/lknmibmhladlajjjiccipambbikcchnf?pli=1)')
-            # input_area = st.empty()
-            # output_area = st.empty()
-            # text = input_area.text_area('Input LD data here')
-
-
-            # def process_ld(text):
-            #     rows = re.split('\t|\n',text)
-            #     if 'Glance views' in rows[0:20]:
-            #         ld = 'ended'
-            #     else:
-            #         ld = 'upcoming'
-                
-            #     asin_indexes = [rows.index(x) for x in rows if re.search('[A-Z0-9]{10}',x)]
-                
-                
-            #     data = []
-            #     for i, item in enumerate(asin_indexes):
-            #         if i < len(asin_indexes)-1:
-            #             data.append(rows[asin_indexes[i]:asin_indexes[i+1]])
-            #         else:
-            #             data.append(rows[asin_indexes[i]:])
-            #     if 'Parent' in data[0][0]:
-            #         data = data[1:]    
-                    
-                
-            #     pattern = re.compile(r'\$|[0-9]+')
-
-            #     for i, d in enumerate(data):
-            #         data[i] = [x for x in d if all([re.search(pattern,x),all(['inventory' not in x, 'Mellanni' not in x])])]
-                    
-            #     result = pd.DataFrame(data)
-            #     asins = result[0].str.split(',', expand = True)
-            #     for col in asins.columns:
-            #         asins[col] = asins[col].str.strip()
-            #     del result[0]
-            #     result = pd.concat([asins, result],axis = 1)
-                
-                
-            #     if ld == 'upcoming':
-            #         if len(result.columns) == 10:
-            #             cols = ['ASIN','SKU','Your price','Deal price','Max Deal price',
-            #                     'Deal price/Max Deal price','Discount, %','Target',
-            #                     'Min Target','Stock']
-            #             num_cols = ['Your price','Deal price','Max Deal price',
-            #                         'Deal price/Max Deal price','Discount, %','Target',
-            #                         'Min Target','Stock']
-                        
-                        
-            #         else:
-            #             cols = ['ASIN','SKU','Your price','Deal price','Max Deal price',
-            #                     'Target',
-            #                     'Min Target','Stock']
-
-            #             num_cols = ['Your price','Deal price','Max Deal price',
-            #                         'Target',
-            #                         'Min Target','Stock']
-                        
-            #         result.columns = cols
-            #         if 'Discount, %' in result.columns.tolist():
-            #             result['Discount, %'] = result['Discount, %'].str.replace('Min: ','')
-                        
-            #         for col in cols:
-            #             result[col] = result[col].str.replace('$','')
-            #         result['Max Deal price'] = result['Max Deal price'].str.replace('Max: ','')
-            #         result['Min Target'] = result['Min Target'].str.replace('Min: ','')
-                    
-            #         for nc in num_cols:
-            #             result[nc] = result[nc].astype(float)
-                        
-            #         result['Discount, %'] = round(((result['Deal price'] / result['Your price']) - 1)*100,1)
-                
-            #         result['Deal price/Max Deal price'] = result['Max Deal price'] - result['Deal price']
-                
-                
-            #     elif ld == 'ended':
-            #         cols = ['ASIN','SKU','Deal price','Sales','Units Sold','Committed units',
-            #                 'Sell-through rate','Glance views','Conversion rate']
-            #         result = result.iloc[:,0:9]
-            #         result.columns = cols
-            #         num_cols = ['Deal price','Sales','Units Sold','Committed units','Glance views']
-            #         for nc in num_cols:
-            #             result[nc] = result[nc].str.replace('$','')
-            #             result[nc] = result[nc].str.replace(',','')
-                        
-            #             result[nc] = result[nc].astype(float)
-            #         result['Sell-through rate'] = round(result['Units Sold'] / result['Committed units'] *100,2)
-            #         result['Conversion rate'] = round(result['Units Sold'] / result['Glance views'] *100,2)
-
-            #     output = BytesIO()
-            #     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-            #         result.to_excel(writer, sheet_name = 'LD_stats', index = False)
-            #         ff.format_header(result,writer,'LD_stats')
-            #     return output.getvalue()
-
-
-            # if st.button('Process LD'):
-            #     try:
-            #         data = process_ld(text)
-            #         st.download_button('Download excel file',data, file_name = 'LD_stats.xlsx')
-            #     except Exception as e:
-            #         output_area.text_area('results',e, label_visibility='hidden')
 
         with st.expander('Business report link generator', icon=':material/monitoring:'):
             business_markets = st.radio('Select marketplace',['US','CA'], horizontal=True)
@@ -266,11 +165,12 @@ if login_st():
             numdays = (end - start).days + 1
             date_range = [end - timedelta(days = x) for x in range(numdays)]
             link_list = []
+            full_list = []
             for d in date_range:
                 link = f"https://sellercentral.amazon.{domain}/business-reports/ref=xx_sitemetric_dnav_xx#/report?id=102%3ADetailSalesTrafficBySKU&chartCols=&columns=0%2F1%2F2%2F3%2F4%2F5%2F6%2F7%2F8%2F9%2F10%2F11%2F12%2F13%2F14%2F15%2F16%2F17%2F18%2F19%2F20%2F21%2F22%2F23%2F24%2F25%2F26%2F27%2F28%2F29%2F30%2F31%2F32%2F33%2F34%2F35%2F36%2F37&fromDate={d.strftime('%Y-%m-%d')}&toDate={d.strftime('%Y-%m-%d')}"
                 link_list.append(link)
                 full_list = '  \n  \n'.join(link_list)
-            if st.button('Generate'):
+            if st.button('Generate') and full_list:
                 st.text_area('Generated links',full_list)
 
         with st.expander('Pricelist checker'):
@@ -381,120 +281,5 @@ if login_st():
         with st.expander('Upload images to web and get direct links', expanded = False, icon=':material/imagesmode:'):
             st.warning(body='This tool has moved to [a separate page](https://mellanni-tools.streamlit.app/Images)', icon=":material/moved_location:")
 
-        with st.expander('Text rewriter', icon=':material/edit_note:'):
-            text_file_obj = st.file_uploader('Upload the excel file with text to work on')
-            if text_file_obj:
-                text_file_sheets = pd.ExcelFile(text_file_obj).sheet_names
-                if text_file_sheets:
-                    sheet = st.selectbox('Select the sheet with data', text_file_sheets)
-                    if sheet:
-                        text_file = pd.read_excel(text_file_obj, sheet_name = sheet)
-                        columns = text_file.columns
-                        col = st.selectbox('Select a column with text to process', columns)
-                        text = text_file[col].values.tolist()
-                        st.write(f'There are {len(text)} blocks to work on')
-            style = st.radio('Select the rewrite power:', ['hard','medium','slight'],horizontal=True, index = 1)
-            if style == 'hard':
-                preprompt = '''Please rewrite the following text, keeping the main idea but using different words and changing the order of sentences, if applicable. Also, please alter the style of the original text'''
-            elif style == 'medium':
-                preprompt = '''Please rewrite the following text, keeping the main idea but using different words and changing the order of sentences, if applicable. Also, please keep the style of the original text'''
-            elif style == 'slight':
-                preprompt = '''Please rewrite the following text just slightly, without greatly altering the style or the order of sentences'''
-
-            prompt = st.text_area('If needs be, please modify the prompt for the bot:',preprompt)
-
-            if st.button('Rewrite'):
-                client = OpenAI(api_key = key)
-                progress_bar = st.progress(len(text)/100,f'Please wait, working on {len(text)} blocks')
-                final_rewrites = pd.DataFrame(columns = ['Original text','Rewritten text'])
-                for i,t in enumerate(text):
-                    messages = [
-                        {'role':'user', 'content':f"{prompt}:\n{t}"}]
-                    try:
-                        response = client.chat.completions.create(
-                        model = model,
-                        messages =  messages,
-                        temperature=0.9,
-                        max_tokens=1000
-                        )
-                        # Get the generated text and append it to the chat history
-                        rewritten = response.choices[0].message.content.strip()
-                        temp = pd.DataFrame([[t,rewritten]], columns = ['Original text','Rewritten text'])
-                        final_rewrites = pd.concat([final_rewrites, temp])
-                    except Exception as e:
-                        st.write(f'Sorry, something went wrong for the following reason\n{e}')
-                    time.sleep(0.5)
-                    progress_bar.progress((i+1)/len(text),f'Rewriting block {i+1} of {len(text)}')
-                st.session_state.output = BytesIO()
-                with pd.ExcelWriter(st.session_state.output, engine='xlsxwriter') as writer:
-                    final_rewrites.to_excel(writer, sheet_name = 'Rewriting', index = False)
-                    ff.format_header(final_rewrites, writer, 'Rewriting')
-                st.download_button('Download results',st.session_state.output.getvalue(), file_name = 'rewrite.xlsx')
-
-
-        with st.expander('Meeting summarizer',expanded = True, icon=':material/summarize:'):
-            used_model = st.radio('Choose model to use:', ['gpt-4o','gpt-4o-mini'], index = 1, horizontal=True)
-            prompt_text = '''
-            Please summarize the following meeting minutes, stay detailed, but concise. Try not to miss any important points.
-            Make sure to explicitly and separately list key talking points (including timestamps) and action items, if any,  in the following format:
-
-            Summary
-
-            Key talking points:
-            - [timestamp] key talking point 1
-            - [timestamp] key talking point 2
-            - etc
-
-            Action items:
-            - <Assignee> : action item 1
-            - <Assignee> : action item 2
-            - etc
-            '''                
-
-            def split_text_to_messages(text):
-                blocks = re.split(r'\n| \.',text)
-                word_limit = 8000 if used_model == 'gpt-4o-mini' else 11000
-                chunks = []
-                limit = 0
-                chunk = []
-                for block in blocks:
-                    limit += len(block.split(' '))
-                    if limit < word_limit:
-                        chunk += [block]
-                    else:
-                        limit = 0
-                        chunks.append(chunk)
-                        chunk = []
-                chunks.append(chunk)
-                return chunks
-
-            def get_meeting_summary(chunks, temp):
-                client = OpenAI(api_key = key)
-                
-                messages = [{'role':'system','content':prompt_text}]
-                messages.extend([{'role':'user', 'content': ' '.join(x)} for x in chunks])
-                response = client.chat.completions.create(
-                model = used_model,
-                messages =  messages,
-                temperature=temp,
-                max_tokens=MAX_TOKENS
-                )
-                #summarize
-                message = response.choices[0].message.content.strip()
-                return message
-            
-            prompt_area = st.empty()
-            text_area = st.empty()
-            clarify_area = st.empty()
-
-            prompt_query = prompt_area.text_area('Prompt',prompt_text,help = 'Modify the query if you are not getting the results you need', height = 150)
-            input_text = text_area.text_area('Input meeting transcription',height = 500)
-            if st.button('Summarize'):
-                st.write(f'using {used_model} for summarization')
-                st.session_state.summarized = True
-                try:
-                    chunks = split_text_to_messages(input_text)
-                    st.session_state.result = get_meeting_summary(chunks, temp = 0.2)
-                except Exception as e:
-                    st.session_state.result = f'Sorry, an error occurred, please contact the administrator\n\n{e}'
-                text_area.text_area('Summary:',st.session_state.result, height = 500)
+        with st.expander('Meeting summarizer',expanded = False, icon=':material/summarize:'):
+            st.warning("Deprecated, use Gemini or any other available chatbot instead", icon=":material/adb:")
